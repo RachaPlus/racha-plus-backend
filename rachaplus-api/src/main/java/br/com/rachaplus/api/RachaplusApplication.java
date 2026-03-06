2725ff82ec4796aaf0c3345829f706de399a56c2
yuri <ymendesqueiroz@gmail.com>
2026-03-06 20:21:51 -0300

    Initial commit

---
+++ rachaplus-api/src/main/java/br/com/rachaplus/api/RachaplusApplication.java
@@ -0,0 +1,13 @@
+package br.com.rachaplus.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RachaplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RachaplusApplication.class, args);
+	}
+
+}
---
+++ rachaplus-api/src/test/java/br/com/rachaplus/api/RachaplusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rachaplus.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RachaplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
