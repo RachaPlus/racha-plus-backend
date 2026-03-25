bdc36de96b0e1ffb46f4e8b992eab8c1c6ddab7b
yuri <ymendesqueiroz@gmail.com>
2026-03-25 01:51:14 -0300

    refactor: torna usuarioService final para garantir imutabilidade

--- rachaplus-api/src/main/java/br/com/rachaplus/api/infrastructure/controller/UsuarioController.java
+++ rachaplus-api/src/main/java/br/com/rachaplus/api/infrastructure/controller/UsuarioController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UsuarioController {
 
-    private UsuarioService usuarioService;
+    private final UsuarioService usuarioService;
 
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
