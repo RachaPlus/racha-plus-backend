266facc182a66b14126733716c20a68296ba37bf
yuri <ymendesqueiroz@gmail.com>
2026-09-18 19:04:39 -0300

    feat: adiciona validacao de tamanho e regex para o username

--- rachaplus-api/src/main/java/br/com/rachaplus/api/application/dto/CadastroUsuarioDTO.java
+++ rachaplus-api/src/main/java/br/com/rachaplus/api/application/dto/CadastroUsuarioDTO.java
@@ -2,10 +2,13 @@ package br.com.rachaplus.api.application.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CadastroUsuarioDTO(
         @NotBlank(message = "O username é obrigatório")
+        @Size(min = 4, max = 16, message = "O username deve ter entre 4 e 16 caracteres")
+        @Pattern(regexp = "^[A-Za-z0-9_.]+$", message = "O username deve conter apenas letras, números, underscores (_) ou pontos (.)")
         String username,
 
         @NotBlank(message = "O email é obrigatório")
