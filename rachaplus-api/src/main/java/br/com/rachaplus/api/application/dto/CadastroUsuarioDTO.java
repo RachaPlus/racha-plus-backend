6f148ef5bddb756f4d6b0d334ee6ed175a0ad417
yuri <ymendesqueiroz@gmail.com>
2026-09-25 16:54:48 -0300

    feat: atualiza politica de senhas para exigir maiuscula e minuscula

--- rachaplus-api/src/main/java/br/com/rachaplus/api/application/dto/CadastroUsuarioDTO.java
+++ rachaplus-api/src/main/java/br/com/rachaplus/api/application/dto/CadastroUsuarioDTO.java
@@ -17,6 +17,6 @@ public record CadastroUsuarioDTO(
 
         @NotBlank(message = "A senha é obrigatória")
         @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$", message = "A senha deve conter pelo menos uma letra e um número")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$", message = "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número")
         String senha
 ) {}
